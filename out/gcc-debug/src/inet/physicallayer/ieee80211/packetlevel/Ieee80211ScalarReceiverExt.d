0749b2f212b299c8d01418e31701d0b30e2f7108
AngeloTrotta85 <angelo@magilia.com>
2017-06-26 15:00:22 +0200

    OK PysicalLayer

---
+++ out/gcc-debug/src/inet/physicallayer/ieee80211/packetlevel/Ieee80211ScalarReceiverExt.d
@@ -0,0 +1,176 @@
+../out/gcc-debug/src/inet/physicallayer/ieee80211/packetlevel/Ieee80211ScalarReceiverExt.o: \
+ inet/physicallayer/ieee80211/packetlevel/Ieee80211ScalarReceiverExt.cc \
+ inet/physicallayer/ieee80211/packetlevel/Ieee80211ScalarReceiverExt.h \
+ /media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/physicallayer/ieee80211/packetlevel/Ieee80211ScalarReceiver.h \
+ /media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/physicallayer/ieee80211/mode/Ieee80211ModeSet.h \
+ /media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/physicallayer/ieee80211/mode/IIeee80211Mode.h \
+ /media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/physicallayer/contract/packetlevel/IModulation.h \
+ /media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/physicallayer/contract/packetlevel/IPrintableObject.h \
+ /media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/physicallayer/base/packetlevel/PhysicalLayerDefs.h \
+ /media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/common/INETMath.h \
+ /media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/common/INETDefs.h \
+ /media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/common/precompiled_debug.h \
+ /media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/common/precompiled.h \
+ /media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/include/omnetpp.h \
+ /media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/opp_defines.h \
+ /media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/features.h \
+ /media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/common/Compat.h \
+ /media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/common/InitStages.h \
+ /media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/common/Units.h \
+ /media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/common/DelayedInitializer.h \
+ /media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/physicallayer/ieee80211/packetlevel/Ieee80211ReceiverBase.h \
+ /media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/physicallayer/base/packetlevel/FlatReceiverBase.h \
+ /media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/physicallayer/base/packetlevel/NarrowbandReceiverBase.h \
+ /media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/physicallayer/base/packetlevel/SNIRReceiverBase.h \
+ /media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/physicallayer/contract/packetlevel/ISNIR.h \
+ /media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/physicallayer/contract/packetlevel/INoise.h \
+ /media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/physicallayer/contract/packetlevel/IReception.h \
+ /media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/physicallayer/contract/bitlevel/ISignalAnalogModel.h \
+ /media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/physicallayer/contract/packetlevel/IRadioSignal.h \
+ /media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/common/geometry/common/Coord.h \
+ /media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/common/mapping/MappingBase.h \
+ /media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/common/mapping/Interpolation.h \
+ /media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/common/mapping/MappingUtils.h \
+ /media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/physicallayer/contract/packetlevel/ITransmission.h \
+ /media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/common/geometry/common/EulerAngles.h \
+ /media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/physicallayer/base/packetlevel/ReceiverBase.h \
+ /media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/physicallayer/contract/packetlevel/IReceiver.h \
+ /media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/physicallayer/contract/packetlevel/IInterference.h \
+ /media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/physicallayer/contract/packetlevel/IListening.h \
+ /media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/physicallayer/contract/packetlevel/IListeningDecision.h \
+ /media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/physicallayer/contract/packetlevel/IReceptionDecision.h \
+ /media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/physicallayer/contract/packetlevel/IReceptionResult.h \
+ /media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/physicallayer/contract/packetlevel/RadioControlInfo_m.h \
+ /media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/physicallayer/contract/packetlevel/IErrorModel.h \
+ /media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/physicallayer/ieee80211/mode/Ieee80211Channel.h \
+ /media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/physicallayer/ieee80211/mode/Ieee80211Band.h \
+ /media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/physicallayer/ieee80211/packetlevel/Ieee80211ControlInfo_m.h \
+ /media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/physicallayer/ieee80211/packetlevel/Ieee80211TransmissionBase.h \
+ /media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/physicallayer/analogmodel/packetlevel/ScalarReception.h \
+ /media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/physicallayer/base/packetlevel/FlatReceptionBase.h \
+ /media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/physicallayer/base/packetlevel/NarrowbandReceptionBase.h \
+ /media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/physicallayer/base/packetlevel/ReceptionBase.h \
+ /media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/physicallayer/contract/packetlevel/IRadio.h \
+ /media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/physicallayer/contract/packetlevel/IPhysicalLayer.h \
+ /media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/physicallayer/contract/packetlevel/IRadioFrame.h \
+ /media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/physicallayer/contract/packetlevel/IArrival.h \
+ /media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/physicallayer/contract/packetlevel/IPhysicalLayerFrame.h \
+ /media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/physicallayer/contract/packetlevel/IAntenna.h \
+ /media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/mobility/contract/IMobility.h \
+ /media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/physicallayer/contract/packetlevel/ITransmitter.h
+
+inet/physicallayer/ieee80211/packetlevel/Ieee80211ScalarReceiverExt.h:
+
+/media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/physicallayer/ieee80211/packetlevel/Ieee80211ScalarReceiver.h:
+
+/media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/physicallayer/ieee80211/mode/Ieee80211ModeSet.h:
+
+/media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/physicallayer/ieee80211/mode/IIeee80211Mode.h:
+
+/media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/physicallayer/contract/packetlevel/IModulation.h:
+
+/media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/physicallayer/contract/packetlevel/IPrintableObject.h:
+
+/media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/physicallayer/base/packetlevel/PhysicalLayerDefs.h:
+
+/media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/common/INETMath.h:
+
+/media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/common/INETDefs.h:
+
+/media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/common/precompiled_debug.h:
+
+/media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/common/precompiled.h:
+
+/media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/include/omnetpp.h:
+
+/media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/opp_defines.h:
+
+/media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/features.h:
+
+/media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/common/Compat.h:
+
+/media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/common/InitStages.h:
+
+/media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/common/Units.h:
+
+/media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/common/DelayedInitializer.h:
+
+/media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/physicallayer/ieee80211/packetlevel/Ieee80211ReceiverBase.h:
+
+/media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/physicallayer/base/packetlevel/FlatReceiverBase.h:
+
+/media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/physicallayer/base/packetlevel/NarrowbandReceiverBase.h:
+
+/media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/physicallayer/base/packetlevel/SNIRReceiverBase.h:
+
+/media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/physicallayer/contract/packetlevel/ISNIR.h:
+
+/media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/physicallayer/contract/packetlevel/INoise.h:
+
+/media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/physicallayer/contract/packetlevel/IReception.h:
+
+/media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/physicallayer/contract/bitlevel/ISignalAnalogModel.h:
+
+/media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/physicallayer/contract/packetlevel/IRadioSignal.h:
+
+/media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/common/geometry/common/Coord.h:
+
+/media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/common/mapping/MappingBase.h:
+
+/media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/common/mapping/Interpolation.h:
+
+/media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/common/mapping/MappingUtils.h:
+
+/media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/physicallayer/contract/packetlevel/ITransmission.h:
+
+/media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/common/geometry/common/EulerAngles.h:
+
+/media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/physicallayer/base/packetlevel/ReceiverBase.h:
+
+/media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/physicallayer/contract/packetlevel/IReceiver.h:
+
+/media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/physicallayer/contract/packetlevel/IInterference.h:
+
+/media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/physicallayer/contract/packetlevel/IListening.h:
+
+/media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/physicallayer/contract/packetlevel/IListeningDecision.h:
+
+/media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/physicallayer/contract/packetlevel/IReceptionDecision.h:
+
+/media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/physicallayer/contract/packetlevel/IReceptionResult.h:
+
+/media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/physicallayer/contract/packetlevel/RadioControlInfo_m.h:
+
+/media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/physicallayer/contract/packetlevel/IErrorModel.h:
+
+/media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/physicallayer/ieee80211/mode/Ieee80211Channel.h:
+
+/media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/physicallayer/ieee80211/mode/Ieee80211Band.h:
+
+/media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/physicallayer/ieee80211/packetlevel/Ieee80211ControlInfo_m.h:
+
+/media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/physicallayer/ieee80211/packetlevel/Ieee80211TransmissionBase.h:
+
+/media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/physicallayer/analogmodel/packetlevel/ScalarReception.h:
+
+/media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/physicallayer/base/packetlevel/FlatReceptionBase.h:
+
+/media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/physicallayer/base/packetlevel/NarrowbandReceptionBase.h:
+
+/media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/physicallayer/base/packetlevel/ReceptionBase.h:
+
+/media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/physicallayer/contract/packetlevel/IRadio.h:
+
+/media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/physicallayer/contract/packetlevel/IPhysicalLayer.h:
+
+/media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/physicallayer/contract/packetlevel/IRadioFrame.h:
+
+/media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/physicallayer/contract/packetlevel/IArrival.h:
+
+/media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/physicallayer/contract/packetlevel/IPhysicalLayerFrame.h:
+
+/media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/physicallayer/contract/packetlevel/IAntenna.h:
+
+/media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/mobility/contract/IMobility.h:
+
+/media/angelo/BigLinux/Programs/OMNeT++/omnetpp-5.1.1/samples/inet/src/inet/physicallayer/contract/packetlevel/ITransmitter.h:
